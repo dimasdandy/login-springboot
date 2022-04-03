03e58c54a5a341306bf5ca9f645121419231989c
dimasdandy <74689960+dimasdandy@users.noreply.github.com>
2022-04-03 15:21:16 +0700

    fixing

--- src/main/java/com/springboot/login/MainSpringBoot.java
+++ src/main/java/com/springboot/login/MainSpringBoot.java
@@ -1,34 +1,9 @@
 package com.springboot.login;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan(basePackages = {
-		"com.springboot.login.base",
-		"com.springboot.login.controller",
-		"com.springboot.login.config",
-		"com.springboot.login.dao",
-		"com.springboot.login.dto.login",
-		"com.springboot.login.security",
-		"com.springboot.login.service",
-		"com.springboot.login.model",
-		"com.springboot.assetmanagement.base",
-		"com.springboot.assetmanagement.controller",
-		"com.springboot.assetmanagement.dao",
-		"com.springboot.assetmanagement.dto",
-		"com.springboot.assetmanagement.model",
-		"com.springboot.assetmanagement.security",
-		"com.springboot.assetmanagement.service"
-})
-@EntityScan(basePackages = {
-		"com.springboot.login.model",
-		"com.springboot.assetmanagement.model"
-})
 public class MainSpringBoot {
 
 	public static void main(String[] args) {
